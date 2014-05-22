c51becefc9ca96761cce6727d19b1baa0f55ab48
ROANNE <jluxyzadluffy@gmail.com>
2014-05-22 13:07:00 +0800

    Update garden.js

--- javascripts/garden.js
+++ javascripts/garden.js
@@ -160,12 +160,12 @@
 	        density: 10,
 	        growSpeed: 1000 / 60,
 	        color: {
-				rmin: 128,
+				rmin: 200,
 				rmax: 255,
-				gmin: 0,
-				gmax: 128,
-				bmin: 0,
-				bmax: 128,
+				gmin: 5,
+				gmax: 55,
+				bmin: 65,
+				bmax: 115,
 	            opacity: 0.1
 	        },
 	        tanAngle: 60
